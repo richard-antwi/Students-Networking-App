24d6d81a1c8672705cd2d83f38f62912d5d033ce
Richard Antwi <richardrichfavourantwi88@gmail.com>
2024-04-10 15:39:32 +0000

    committing updates on signup and login

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,6 +1,6 @@
-import React, { useState} from 'react';
 import axios from 'axios';
 import 'bootstrap';
+import React, { useState } from 'react';
 import "../App.css";
 
 
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -4,51 +4,49 @@ const validator = require('validator');
 
 const UserSchema = new mongoose.Schema(
   {
-  firstName:{ 
-    type: String, 
-    required: true
-   },
-  lastName: { 
-    type: String, 
-    required: true 
-  },
-  userName: { 
-    type: String, 
-    required: true, 
-    unique: true 
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true, // Converts email to lowercase
-    validate: [validator.isEmail, 'Please provide a valid email']
-  },
-  
-  password: { 
-    type: String, 
-    required: true 
-  },
-  dateOfBirth: { 
-    type: Date, 
-    required: true 
-  },
-  selectProgram: { 
-    type: String, 
-    required: false 
-  },
-  university: { 
-    type: String, 
-    required: false 
-  },
-  profilePicture: { 
-    type: String, 
-    required: false 
-  },
-}, { timestamps: true }); // Using timestamps option to auto-generate createdAt and updatedAt fields
+    firstName: { 
+      type: String, 
+      required: true
+    },
+    lastName: { 
+      type: String, 
+      required: true 
+    },
+    userName: { 
+      type: String, 
+      required: true, 
+      unique: true 
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    password: { 
+      type: String, 
+      required: true 
+    },
+    dateOfBirth: { 
+      type: Date, 
+      required: true 
+    },
+    selectProgram: { 
+      type: String
+    },
+    university: { 
+      type: String
+    },
+    profilePicture: { 
+      type: String
+    },
+  }, 
+  { timestamps: true }
+);
 
 // Hash the password before saving
-userSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function(next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 12);
   }
@@ -56,10 +54,10 @@ userSchema.pre('save', async function(next) {
 });
 
 // Method to compare candidate password with user's password
-userSchema.methods.comparePassword = async function(candidatePassword) {
+UserSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;

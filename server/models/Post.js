98c6a5f9367e6d6a209adb586b1fdc44ee778f4d
Richard Antwi <richardrichfavourantwi88@gmail.com>
2024-05-27 21:58:56 +0000

    updates on post

---
+++ server/models/Post.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the Post schema
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  postedAt: {
+    type: Date,
+    default: Date.now
+  },
+  interests: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  jobTitle: {
+    type: String,
+    required: true
+  },
+  jobType: {
+    type: String,
+    required: true
+  },
+  hourlyRate: {
+    type: Number,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  tags: [{
+    type: String,
+    required: true
+  }],
+  likes: {
+    type: Number,
+    default: 0
+  },
+  comments: {
+    type: Number,
+    default: 0
+  },
+  views: {
+    type: Number,
+    default: 0
+  }
+});
+
+// Create the Post model
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;

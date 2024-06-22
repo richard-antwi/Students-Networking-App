740564a842d49754b754a90afba2fb15651c5e38
Richard Antwi <richardrichfavourantwi88@gmail.com>
2024-06-22 22:03:54 +0000

    updates on program

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.use((err, req, res, next) => {
   } 
 });
 
-// Start the server
+// Start the server on port 3001
 app.listen(process.env.PORT || 3001, () => {
     console.log(`Server is running on port ${process.env.PORT || 3001}`);
 });
